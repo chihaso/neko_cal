45a7d3b62d99d17998b8a0f73323eb260fe2a43e
chihaso <cycyccq@gmail.com>
2022-10-31 22:55:34 +0900

    記録対象をSPAっぽく新規登録できるようにする

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class SubjectsController < ApplicationController
-  before_action :set_subject
+  before_action :set_subject, except: %i[new create]
+
+  def new
+    @cat = Cat.find(params[:cat_id])
+    @subject = @cat.subjects.new
+  end
+
+  def create
+    @cat = Cat.find(params[:subject][:cat_id])
+    @subject = @cat.subjects.new(subject_params)
+
+    if @subject.save
+      flash.now.notice = I18n.t('subjects.successfully_created', subject: @subject.name)
+    else
+      render :new, status: :unprocessable_entity, alart: I18n.t('subjects.failed_to_create')
+    end
+  end
 
   def edit; end
 
