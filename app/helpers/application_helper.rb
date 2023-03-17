bdf61ed0f4c7b7f0899cbfbe3669e58701c51735
chihaso <cycyccq@gmail.com>
2023-03-17 22:38:56 +0900

    記録方式を表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def render_recording_method_setting(content)
+    case content.recording_method
+    when RecordingMethod::MultiLevel
+      render('/recording_methods/multi_level', resource: content.recording_method)
+    end
+  end
 end
